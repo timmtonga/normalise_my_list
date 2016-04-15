05413907952114ff2d8522ca6c9cee198cecf771
Timothy Mtonga <timmtonga@gmail.com>
2016-04-15 13:06:27 -0400

    New clean data

--- scripts/exact_matching.rb
+++ scripts/exact_matching.rb
@@ -1,24 +1,25 @@
 def start
 	terms = []
 
-	CSV.foreach("#{Rails.root}/data/2016-04-12-drug-list.csv") do |row|
-
+	CSV.foreach("#{Rails.root}/data/meml_2009_final.csv") do |row|
 		terms << row[0].squish
-
 	end
+	count = 0
+	total = terms.length
 
-	
-	CSV.open("#{Rails.root}/output/exact_matching_#{Time.now}.csv", "wb") do |csv|
-		terms.uniq.each  do |item|
+	CSV.open("#{Rails.root}/output/exact_matching_#{Time.now.strftime('%Y%d%b%H%M')}.csv", "wb") do |csv|
+
+		(terms || []).each  do |item|
 
 			w = Rxnconso.where("STR = ?",item)
 			if !w.blank?
 				csv << [item, w.collect{|x| x.RXAUI}, w.collect{|x| x.STR}]
 			end
-		end
-		
-	end 
 
+			count += 1
+			puts "#{count} out of #{total}"
+		end
+	end
 end
 
 start
